c65d867d87dd75d0b3d430ae236da63fc1d79ffc
user <user@user>
2023-07-27 20:04:01 +0200

    Update Lib_CSharp and Program.cs

--- Echo/Main/src/Program.cs
+++ Echo/Main/src/Program.cs
@@ -1,12 +1,25 @@
 ﻿
-
 using PoliNetwork.Telegram.Objects;
+using PoliNetwork.Telegram.Utils;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
 
 Console.WriteLine("Hello, starting Echo bot!");
 
 var telegramBotWrapper = new TelegramBotWrapper("token");
 
+telegramBotWrapper.Start(HandleUpdateAsync);
 while (true)
 {
-    telegramBotWrapper.CheckUpdatesAndRun();
+    Console.ReadLine();
+}
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    // Only process Message updates: https://core.telegram.org/bots/api#message
+    if (update.Message is not { } message)
+        return;
+    
+    await Echo.EchoMethod(message, botClient, cancellationToken);
 }
