ec10d8d0423b4cbcda344cfd2128635ce757e54e
Eliaxie <elia.maggioni@gmail.com>
2023-07-28 00:24:47 +0200

    added tests fixed ci

--- Echo/Main/src/Program.cs
+++ Echo/Main/src/Program.cs
@@ -7,7 +7,7 @@ using Telegram.Bot.Types;
 
 Console.WriteLine("Hello, starting Echo bot!");
 
-var telegramBotWrapper = new TelegramBotWrapper("token");
+var telegramBotWrapper = new TelegramBot("token");
 
 telegramBotWrapper.Start(HandleUpdateAsync);
 while (true)
@@ -20,6 +20,6 @@ async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, Cancel
     // Only process Message updates: https://core.telegram.org/bots/api#message
     if (update.Message is not { } message)
         return;
-    
+
     await Echo.EchoMethod(message, botClient, cancellationToken);
 }
--- Echo/Test/UnitTest1.cs
+++ Echo/Test/UnitTest1.cs
@@ -1,3 +1,9 @@
+using NUnit.Framework.Internal;
+using PoliNetwork.Telegram.Objects;
+using PoliNetwork.Telegram.Utils;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
 namespace EchoTest;
 
 public class Tests
@@ -10,6 +16,10 @@ public class Tests
     [Test]
     public void Test1()
     {
+        var telegramBotWrapper = new TelegramBot("token");
+        telegramBotWrapper.Start((ITelegramBotClient botClient, Update update, CancellationToken cancellationToken) => { return Task.CompletedTask; });
+
         Assert.Pass();
+
     }
 }
