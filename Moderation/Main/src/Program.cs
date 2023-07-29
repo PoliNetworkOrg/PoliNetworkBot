305f6da84cb0c944490485f1b1583198da16ccc4
herfost <133514023+sbadung@users.noreply.github.com>
2023-07-29 17:22:48 +0200

    implementation specifics on issue #002

--- Moderation/Main/src/Program.cs
+++ Moderation/Main/src/Program.cs
@@ -1,6 +1,6 @@
 using PoliNetwork.Core.Data;
 using PoliNetwork.Core.Utils;
-using PoliNetwork.Core.Utils.LoggerNS;
+using PoliNetwork.Telegram.Logger;
 using PoliNetwork.Db.Utils;
 using PoliNetwork.Telegram.Objects.Bot;
 using PoliNetwork.Telegram.Utils;
@@ -8,59 +8,53 @@ using PoliNetwork.Telegram.Utils.ConfigUtils;
 using PoliNetwork.Telegram.Variables;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using PoliNetwork.Core.Utils.LoggerNS;
+using System.Configuration;
 
 namespace Moderation;
 
 internal static class Program
 {
-    /// <summary>
-    ///     Telegram bot
-    /// </summary>
-    private static TelegramBot? _telegramBot;
+    private static AbstractTelegramBot? bot = null;
+    private static AbstractLogger.LogConfiguration? loggerConfiguration = null;
 
-    /// <summary>
-    ///     Default log config
-    /// </summary>
-    private static readonly LogConfig LogConfig = new();
-
-
-    /// <summary>
-    ///     Handle updates
-    /// </summary>
-    /// <param name="botClient">botClient</param>
-    /// <param name="update">update</param>
-    /// <param name="cancellationToken">cancellationToken</param>
-    private static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update,
-        CancellationToken cancellationToken)
+    private static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
-        if (_telegramBot == null
-            || botClient.BotId != _telegramBot.GetId()
+        if (bot == null
+            || botClient.BotId != bot.BotId
             || update.Message is not { } message)
             return; // https://core.telegram.org/bots/api#message
 
-        await Echo.EchoMethod(message, _telegramBot, cancellationToken);
+        await Echo.EchoMethod(message, bot, cancellationToken);
     }
 
     public static void Main(string[] args)
     {
-        GlobalVariables.DefaultLogger.SetLogConfing(LogConfig);
-        GlobalVariables.DefaultLogger.Info("Hello, starting Moderation bot!");
-        var telegramConfig = TelegramConfigUtils.LoadOrInitializeConfig(Variables.DefaultConfigPath);
-        var dbConfig = DbConfigUtils.LoadOrInitializeConfig(PoliNetwork.Db.Variables.Variables.DefaultConfigPath);
-        if (telegramConfig == null)
+
+        // The PoliNetwork.Core.Utils.LoggerNS.Logger must extends the AbstractLogger so a sub class will be needed
+        // The only dependency of the PoliNetwork.Core.Utils.LoggerNS.Logger resides here in the Program.cs 
+        AbstractLogger? logger = null; // = new PoliNetwork.Core.Utils.LoggerNS.Logger();
+
+        TelegramConfigUtils.FileConfigurationLoader fileConfigurationLoader = new TelegramConfigUtils.JSONFileConfigurationLoader();
+        logger?.Info("Hello, starting Moderation bot!");
+        var botConfiguration = fileConfigurationLoader.LoadOrInitializeConfig(Variables.DefaultConfigPath);
+        var databaseConfiguration = DbConfigUtils.LoadOrInitializeConfig(PoliNetwork.Db.Variables.Variables.DefaultConfigPath);
+        if (botConfiguration == null)
         {
             GlobalVariables.DefaultLogger.Emergency("Telegram Config is undefined when starting the bot.");
             return;
         }
 
-        if (dbConfig == null)
+        if (databaseConfiguration == null)
         {
             GlobalVariables.DefaultLogger.Emergency("Database Config is undefined when starting the bot.");
             return;
         }
 
-        _telegramBot = new TelegramBot(telegramConfig, LogConfig);
-        _telegramBot.Start(HandleUpdateAsync);
+        // TelegramConfiguration should extends TelegramBotClientOptions 
+        TelegramBotClientOptions options = new(botConfiguration.Token, botConfiguration.BaseUrl);
+        bot = new ModerationBot(options: options, logger: logger);
+        bot.Start(HandleUpdateAsync);
         Wait.WaitForever();
     }
 }
--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # PoliNetworkBot
 Repository for the new revision of https://github.com/PoliNetworkOrg/PoliNetworkBot_CSharp
-
-### Lib
-
 We are using this library https://github.com/PoliNetworkOrg/Lib_CSharp
