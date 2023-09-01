e8b9d853f64f27169a0d7bbce81aad36d5a1ed32
angeousta <angeousta@outlook.it>
2023-09-01 11:31:48 +0200

    .

--- src/Configuration.cs
+++
@@ -1,9 +0,0 @@
-namespace Configuration
-{
-  public sealed class Config
-  {
-    public static readonly string FORBIDDEN_WORDS = @"/persistence/forbidden_words.json";
-    public static readonly string APP_SETTIGNS = @"./appsettings.json";
-  }
-
-}
