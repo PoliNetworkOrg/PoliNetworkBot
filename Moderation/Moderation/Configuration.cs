c3115389e0530af4b70a0a5757b0acaaa0dad40a
oneeyedoll <efallletti@gmail.com>
2023-09-01 18:33:35 +0200

    restructured file structure

--- Moderation/Moderation/Configuration.cs
+++ Moderation/Moderation/Configuration.cs
@@ -2,8 +2,8 @@ namespace Configuration
 {
   public sealed class Config
   {
-    public static readonly string FORBIDDEN_WORDS = @"/persistence/forbidden_words.json";
-    public static readonly string APP_SETTIGNS = "appsettings.json";
+    public static readonly string FORBIDDEN_WORDS = @"..\persistence\forbidden_words.json";
+    public static readonly string APP_SETTIGNS = @"..\persistence\appsettings.json";
   }
 
 }
