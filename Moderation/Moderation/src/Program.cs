fc128d6e68f0e1f8410d552598f7a72ad3285096
joebiden <133514023+sbadung@users.noreply.github.com>
2023-09-07 16:27:43 +0200

    Revert "Fixing issue#14 actions and build"
    This reverts commit b52c38d5fe911dd04ccc21107049f0fc81165a80.

--- Moderation/Moderation/src/Program.cs
+++ Moderation/Moderation/src/Program.cs
@@ -1,29 +1,56 @@
-﻿using PoliNetwork.Telegram.Bot;
-using PoliNetwork.Telegram.Bot.Functionality;
-using PoliNetwork.Telegram.Bot.Functionality.Example;
-using PoliNetwork.Telegram.Bot.Handler;
-using PoliNetwork.Utility.ConfigurationLoader;
-using PoliNetwork.Utility.Configuration;
+﻿#region 
+using PoliNetwork.Core.Utils;
+using PoliNetwork.Telegram.Logger;
+using PoliNetwork.Db.Utils;
+using PoliNetwork.Telegram.Configuration;
+using PoliNetwork.Telegram.ConfigurationLoader;
+using PoliNetwork.Db.Objects;
+using PoliNetwork.Telegram.Bot.Bots;
+using PoliNetwork.Telegram.Options;
+using PoliNetwork.Telegram.Properties;
+#endregion 
 
-namespace PoliNetwork.Moderation
+namespace Moderation;
+
+internal static class Program
 {
-    internal abstract class Program
+    private static ModerationBot? _bot = null;
+    private const string DatabaseConfigurationPath = "";
+
+    public static void Main()
     {
-        private static async Task Main()
+        /* The PoliNetwork.Core.Utils.LoggerNS.Logger must extends the AbstractLogger so a sub class will be needed
+        The only dependency of the PoliNetwork.Core.Utils.LoggerNS.Logger resides here in the Program.cs  */
+        AbstractLogger? logger = new DefaultLogger();
+        FileConfigurationLoader fileConfigurationLoader = new JSONFileConfigurationLoader();
+        AbstractTelegramBotOptions? botConfiguration = fileConfigurationLoader.LoadOrInitializeConfig(Configuration.TELEGRAM_CONFIGURATION_PATH, logger);
+
+        DbConfig? databaseConfiguration = DbConfigUtils.LoadOrInitializeConfig(DatabaseConfigurationPath);
+        if (botConfiguration == null)
         {
-            List<ITelegramBotFunctionality> telegramBotFunctionalities = new()
-            {
-                new ResponseOnMessageFunctionality()
-            };
-
-            IUpdateHandler updateHandler = new DefaultUpdateHandler(telegramBotFunctionalities);
-            IPollingErrorHandler pollingErrorHandler = new DefaultPollingErrorHandler();
-
-            var botToken = new JSONConfigurationLoader().LoadConfiguration(Config.APP_SETTINGS)
-                .GetSection("Secrets:BotToken").Value!;
-            TelegramBot bot = new(botToken);
-
-            await bot.RunAsync(updateHandler, pollingErrorHandler);
+            logger?.Emergency("Telegram Config is undefined when starting the bot.");
+            return;
         }
+
+        if (databaseConfiguration == null)
+        {
+            logger?.Emergency("Database Config is undefined when starting the bot.");
+            return;
+        }
+
+        logger?.Info("RUNNING MODERATION BOT");
+        botConfiguration.UpdateMethod = new UpdateMethod(UpdateAction);
+
+        /* TelegramConfiguration should extends TelegramBotClientOptions */
+        _bot = new ModerationBot(options: botConfiguration, logger: logger);
+        _bot.Start(new CancellationToken());
+        Wait.WaitForever();
+    }
+
+    private static void UpdateAction(CancellationToken cancellationToken, IUpdate update)
+    {
+        /* Process Message updates only, see: https://core.telegram.org/bots/api#message */
+        if (_bot == null || update.Message is not { } message) return;
+        _bot.Echo(message, cancellationToken);
     }
 }
