2da4c7764a00444251c4ac8fc77a5f2412ce1037
joebiden <133514023+sbadung@users.noreply.github.com>
2023-09-07 21:02:25 +0200

    Merge conflict from main #1

--- Moderation/Moderation/Configuration.cs
+++
@@ -1,9 +0,0 @@
-namespace Configuration
-{
-  public sealed class Config
-  {
-    public static readonly string FORBIDDEN_WORDS = @"\moderation\persistence\forbidden_words.json";
-    public static readonly string APP_SETTIGNS = @"\moderation\persistence\appsettings.json";
-  }
-
-}
